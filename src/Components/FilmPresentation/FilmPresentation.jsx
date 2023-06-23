48d78cb2ee365b8b64d9b7fc1c5ad753d53f50db
EShroomThe1st <xxxthedestroyer058xxx@gmail.com>
2023-06-23 18:08:53 +0700

    Lab1

---
+++ src/Components/FilmPresentation/FilmPresentation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './FilmPresentation.module.css'
+import { Films } from '../../Map/Map'
+import { Container } from 'react-bootstrap'
+
+const FilmPresentation = () => {
+  return (
+    <Container style={{display: "grid", gridTemplateColumns:"1fr 1fr 1fr", marginLeft:"7rem"}}>
+        {Films.map((films) => (
+            <div className={styles.Film}>
+                <img src={films.Image}/>
+                <div className={styles.Content}>
+                    <p style={{fontSize:"1.5rem"}}>{films.Title}</p>
+                    <p style={{fontSize:"1.25rem"}}>{films.Year}</p>
+                    <p style={{fontSize:"1rem"}}>{films.Nation}</p>
+                </div>
+            </div>
+        ))}
+    </Container>
+  )
+}
+
+export default FilmPresentation
---
+++ src/Map/Map.js
@@ -0,0 +1,60 @@
+
+export const Films=[
+    {
+    Image: "asset/img/HarryPotter1.jpg" ,
+    Title: "Harry Potter and the Sorcerer's Stone",
+    Year: "2001",
+    Nation: "USA",
+    },
+
+    {
+        Image: "asset/img/No-Country-For-Old-Men.jpg" ,
+        Title: "No Country for Old Man",
+        Year: "2007",
+        Nation: "USA",
+    },
+
+    {
+            Image: "asset/img/Breaking-Bad.webp" ,
+            Title: "Breaking Bad",
+            Year: "2008",
+            Nation: "USA",
+    },
+
+    {
+        Image: "asset/img/7-Samurai_.jpg" ,
+        Title: "7 Samurai",
+        Year: "1954",
+        Nation: "Japan",
+    },
+    {
+        Image: "asset/img/Oldboy.jpg" ,
+        Title: "Old Boy",
+        Year: "2003",
+        Nation: "Korean",
+    },
+    {
+        Image: "asset/img/Saving-Private-Ryan.jpg" ,
+        Title: "Saving Private Ryan",
+        Year: "1998",
+        Nation: "USA",
+    },
+    {
+        Image: "asset/img/Better-Call-Saul.jpg" ,
+        Title: "Better Call Saul",
+        Year: "2015",
+        Nation: "USA",
+    },
+    {
+        Image: "asset/img/Puss-in-Boots.jpg" ,
+        Title: "Puss in Boots",
+        Year: "2022",
+        Nation: "USA",
+    },
+    {
+        Image: "asset/img/3-idiots.jpg" ,
+        Title: "3 Idiots",
+        Year: "2009",
+        Nation: "India",
+    },
+]
